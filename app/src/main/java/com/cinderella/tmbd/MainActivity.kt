3fd01e9c9e9d6838df5bc2eea668b7266c49bf97
xidl <26184016+xidsyed@users.noreply.github.com>
2024-01-24 11:29:07 +0530

    Added Top and Bottom Bar with Nested NavHosts

--- app/src/main/java/com/cinderella/tmbd/MainActivity.kt
+++ app/src/main/java/com/cinderella/tmbd/MainActivity.kt
@@ -1,66 +1,77 @@
 package com.cinderella.tmbd
 
 import android.os.Bundle
-import android.os.Debug
 import android.util.Log
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.State
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.repeatOnLifecycle
-import com.cinderella.tmbd.movieList.presentation.MovieListState
-import com.cinderella.tmbd.movieList.presentation.MovieListUiEvent
-import com.cinderella.tmbd.movieList.presentation.MovieListViewModel
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.cinderella.tmbd.movieList.presentation.HomeScreen
+import com.cinderella.tmbd.movieList.util.Screen
 import com.cinderella.tmbd.ui.theme.TMBDTheme
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOn
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TMBDTheme {
+                SetBarColor(color = MaterialTheme.colorScheme.inverseOnSurface)
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    val viewModel = hiltViewModel<MovieListViewModel>()
-                    val lifecycleOwner = LocalLifecycleOwner.current
-                    var state = viewModel.movieListState.collectAsStateWithLifecycle()
-                    MovieListScreen(state = state, onUiEvent = viewModel::onUiEvent)
+                    val navController = rememberNavController()
+                    NavHost(navController, startDestination = Screen.Home.route) {
+                        composable(Screen.Home.route) {
+                            HomeScreen()
+                        }
+                        composable(
+                            route = Screen.Details.route + "/{movieId}",
+                            arguments = listOf(
+                                navArgument("movieId") { type = NavType.IntType }
+                            )) {
+                            val movieId = it.arguments?.getString("movieId")
+                            // DetailScreen (movieId)
+                        }
+                    }
                 }
             }
         }
     }
 
     override fun onResume() {
-        super.onResume();
+        super.onResume()
         if (BuildConfig.DEBUG) { // don't even consider it otherwise
-                Log.d("SCREEN", "Keeping screen on for debugging, detach debugger and force an onResume to turn it off.");
-                getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            Log.d(
+                "SCREEN",
+                "Keeping screen on for debugging, detach debugger and force an onResume to turn it off."
+            )
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         }
     }
 }
 
 @Composable
-fun MovieListScreen(state: State<MovieListState>, onUiEvent: (MovieListUiEvent) -> Unit) {
-    Log.d("TAG", "MovieListScreen: ${state.value.popularMovieList}")
-    when(state.value) {
-
+private fun SetBarColor(color: Color) {
+    val systemUiController = rememberSystemUiController()
+    LaunchedEffect(key1 = color) {
+        systemUiController.setSystemBarsColor(color)
     }
 }
 
---
+++ app/src/main/java/com/cinderella/tmbd/movieList/presentation/HomeScreen.kt
@@ -0,0 +1,146 @@
+package com.cinderella.tmbd.movieList.presentation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Movie
+import androidx.compose.material.icons.rounded.Upcoming
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.cinderella.tmbd.R
+import com.cinderella.tmbd.movieList.util.Screen
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen () {
+    val viewModel = hiltViewModel<MovieListViewModel>()
+    val state = viewModel.movieListState.collectAsStateWithLifecycle().value
+    val navController = rememberNavController()
+    Scaffold(topBar = {
+        TopAppBar(
+            title = {
+                Text(
+                    text = if (state.isCurrentPopularScreen) stringResource(R.string.popular_movies)
+                    else stringResource(R.string.upcoming_movies), fontSize = 20.sp
+                )
+            },
+            modifier = Modifier.shadow(2.dp),
+            colors = TopAppBarDefaults.smallTopAppBarColors(
+                MaterialTheme.colorScheme.inverseOnSurface
+            )
+        )
+    }, bottomBar = {
+        BottomNavigationBar(
+            bottomNavController = navController,
+            onEvent = viewModel::onUiEvent
+        )
+    }) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) {
+            NavHost(
+                navController = navController,
+                startDestination = Screen.PopularMovieList.route
+            ) {
+                composable(Screen.PopularMovieList.route) {
+                    // MoviesListScreen(Popular)
+                }
+                composable(Screen.UpcomingMovieList.route) {
+                    // MoviesListScreen(Upcoming)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BottomNavigationBar(
+    bottomNavController: NavHostController, onEvent: (MovieListUiEvent.Navigate) -> Unit
+) {
+    val items = listOf(
+        BottomNavItem(stringResource(R.string.popular), Icons.Rounded.Movie),
+        BottomNavItem(stringResource(R.string.upcoming), Icons.Rounded.Upcoming),
+    )
+
+    var selectedIndex by rememberSaveable { mutableStateOf(0) }
+
+    NavigationBar {
+        Row(
+            modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            items.forEachIndexed { index, bottomNavItem ->
+                Add(bottomNavItem, index, selectedIndex) {
+                    selectedIndex = index
+                    onEvent(MovieListUiEvent.Navigate)
+                    when (index) {
+                        0 -> {
+                            bottomNavController.popBackStack()
+                            bottomNavController.navigate(Screen.PopularMovieList.route)
+                        }
+
+                        1 -> {
+                            bottomNavController.popBackStack()
+                            bottomNavController.navigate(Screen.UpcomingMovieList.route)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun RowScope.Add(
+    navItem: BottomNavItem,
+    index: Int,
+    selectedIndex: Int,
+    onClick : () -> Unit
+) {
+    NavigationBarItem(icon = {
+        Icon(
+            imageVector = navItem.icon,
+            contentDescription = navItem.title
+        )
+    },
+        label = { Text(navItem.title) },
+        selected = index == selectedIndex,
+        onClick = { onClick() }
+    )
+
+}
+
+data class BottomNavItem(
+    val title: String, val icon: ImageVector
+)
